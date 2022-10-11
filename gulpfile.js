f009e19f6737966fb394f46a875076776532f83c
Anastasia Kiseleva <aakkiseleva@gmail.com>
2022-10-11 19:04:50 +0300

    First commit

---
+++ gulpfile.js
@@ -0,0 +1,186 @@
+import gulp from 'gulp';
+import less from 'gulp-less';
+import lessGlob from 'less-plugin-glob';
+import plumber from 'gulp-plumber';
+import postcss from 'gulp-postcss';
+import csso from 'postcss-csso';
+import autoprefixer from 'autoprefixer';
+import browserSync from 'browser-sync';
+import rename from 'gulp-rename';
+import svgmin from 'gulp-svgmin';
+import svgSprite from 'gulp-svg-sprite';
+import terser from 'gulp-terser';
+import squoosh from 'gulp-libsquoosh';
+import del from 'del';
+import htmlmin from 'gulp-htmlmin';
+// import gcmq from 'gulp-group-css-media-queries';
+import webpackStream from 'webpack-stream';
+import ghpages from 'gh-pages';
+
+// Заливка на git hub pages
+export const pages = (cb) => ghpages.publish('./build', cb);
+
+// Scripts
+export const script = () => gulp.src('source/scripts/main.js')
+  .pipe(webpackStream({
+    mode: 'production',
+    output: {
+      filename: 'main.js',
+    },
+    module: {
+      rules: [
+        {
+          test: /\.(js)$/,
+          exclude: /(node_modules)/,
+          loader: 'babel-loader'
+        }
+      ]
+    }
+  }))
+  .pipe(plumber())
+  .pipe(terser())
+  .pipe(rename({ suffix: '.min' }))
+  .pipe(gulp.dest('./build/scripts'));
+
+// Styles
+
+export const styles = () => gulp.src('source/styles/styles.less', { sourcemaps: true })
+  .pipe(plumber())
+  .pipe(less({
+    plugins: [lessGlob]
+  }))
+  .pipe(postcss([
+    autoprefixer(),
+    csso()
+  ]))
+  .pipe(rename('styles.min.css'))
+  .pipe(gulp.dest('build/css', { sourcemaps: '.' }))
+  .pipe(browserSync.stream());
+
+// HTML
+
+const html = () => gulp.src('source/*.html')
+  .pipe(htmlmin({ collapseWhitespace: true }))
+  .pipe(gulp.dest('build'));
+
+// Images
+
+const optimizeImages = () => gulp.src('source/img/**/*.{jpg,png}')
+  .pipe(squoosh())
+  .pipe(gulp.dest('build/images'));
+
+const copyImages = () => gulp.src('source/img/**/*.{jpg,png}')
+  .pipe(gulp.dest('build/images'));
+
+// WebP
+
+const createWebp = () => gulp.src(['source/img/**/*.{jpg,png}', '!source/img/favicons/*{png,svg}'])
+  .pipe(squoosh({
+    webp: {}
+  }))
+  .pipe(gulp.dest('build/images'));
+
+// SVG
+
+const svg = () => gulp.src(['source/img/**/*.svg'])
+  .pipe(svgmin())
+  .pipe(gulp.dest('build/images'));
+
+// SVG Sprite
+
+const config = {
+  mode: {
+    stack: true
+  }
+};
+
+export const svgSprites = () => gulp.src('source/svg-sprites/*.svg')
+  .pipe(svgSprite(config))
+  .pipe(rename('sprite.svg'))
+  .pipe(gulp.dest('build/images/svg-sprites'));
+
+// Copy fonts, ico, webmanifest
+
+export const copy = (done) => {
+  gulp.src([
+    'source/fonts/*woff2',
+    'source/*.ico',
+    'source/*.webmanifest'
+  ], {
+    base: 'source'
+  })
+    .pipe(gulp.dest('build'));
+  done();
+};
+
+// Clean
+
+const clean = () => del('build');
+
+// Server
+
+const server = (done) => {
+  browserSync.init({
+    server: {
+      baseDir: 'build'
+    },
+    cors: true,
+    notify: false,
+    ui: false,
+    online: true
+  });
+  done();
+};
+
+// Reload
+
+const reload = (done) => {
+  browserSync.reload();
+  done();
+};
+
+// Watcher
+
+const watcher = () => {
+  gulp.watch([
+    './source/styles/**/*.less',
+    './source/blocks/**/*.less'
+  ], gulp.series(styles));
+  gulp.watch([
+    'source/*.html',
+  ], gulp.series(html, reload));
+  gulp.watch([
+    'source/blocks/**/*.js',
+    'source/scripts/**/*.js'
+  ], gulp.series(script, reload));
+};
+
+export default gulp.series(
+  clean,
+  copy,
+  copyImages,
+  gulp.parallel(
+    styles,
+    html,
+    script,
+    createWebp,
+    svg,
+    svgSprites
+  ),
+  gulp.series(
+    server,
+    watcher
+  ));
+
+export const build = gulp.series(
+  clean,
+  copy,
+  optimizeImages,
+  gulp.parallel (
+    styles,
+    html,
+    script,
+    svg,
+    svgSprites,
+    createWebp
+  ), pages);
